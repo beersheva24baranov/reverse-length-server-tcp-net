7ebb07d4a28fda41cefe07ce44b196e18b4560a7
AlexanderBaranov <avbc@mail.ru>
2024-11-06 21:02:46 +0200

    some fixes

--- reverse-length-client-tcp-net/src/main/java/net/Main.java
+++ reverse-length-client-tcp-net/src/main/java/net/Main.java
@@ -1,7 +1,46 @@
 package net;
+import java.io.*;
+import view.*;
 
 public class Main {
+    static TcpClient client;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Menu menu = new Menu("TCP Network application",
+                Item.of("Start session", Main::startSession),
+                Item.of("Exit", Main::exit, true));
+        menu.perform(new StandardInputOutput());
+    }
+
+    static void startSession(InputOutput io) {
+        String host = io.readString("Enter hostname");
+        int port = io.readNumberRange("Enter port (9000-64000)", "Wrong port", 9000, 64000).intValue();
+        tryingToClose();
+        client = new TcpClient(host, port);
+        Menu menu = new Menu("Run Session",
+                Item.of("Enter command and data", Main::stringProcessing),
+                Item.ofExit());
+        menu.perform(io);
+    }
+
+    static void stringProcessing(InputOutput io) {
+        String requestType = io.readString("Enter your command (reverse, lenght)");
+        String requestData = io.readString("Enter your data");
+        String response = client.sendAndReceive(requestType, requestData);
+        io.writeLine(response);
+    }
+
+    static void exit(InputOutput io) {
+        tryingToClose();
+    }
+
+    private static void tryingToClose() {
+        if (client != null) {
+            try {
+                client.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
