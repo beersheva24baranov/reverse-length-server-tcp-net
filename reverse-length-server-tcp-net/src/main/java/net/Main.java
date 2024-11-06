c7fde2b3ca328230250a65fe30e5287124741867
AlexanderBaranov <avbc@mail.ru>
2024-11-06 21:07:21 +0200

    some fixes

--- reverse-length-server-tcp-net/src/main/java/net/Main.java
+++ reverse-length-server-tcp-net/src/main/java/net/Main.java
@@ -1,7 +1,26 @@
 package net;
-
 public class Main {
+    private static final int SERVER_PORT = 9500;
+
+    static Protocol protocol = new Protocol() {
+        @Override
+        public Response getResponse(Request request) {
+            String requestData = request.requestData();
+            ResponseCode responseCode = ResponseCode.OK;
+            String responseData = switch (request.requestType()) {
+                case "reverse" -> new StringBuilder(requestData).reverse().toString();
+                case "length" -> String.valueOf(requestData.length());
+                default -> {
+                    responseCode = ResponseCode.WRONG_TYPE;
+                    yield "server doesn't understand your command";
+                }
+            };
+            return new Response(responseCode, responseData);
+        }
+    };
+    static TcpServer server = new TcpServer(protocol, SERVER_PORT);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        server.run();
     }
 }
